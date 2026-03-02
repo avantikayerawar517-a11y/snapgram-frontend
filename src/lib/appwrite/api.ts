932eb6965554592477882690e95407e24d4fb3c4
Avantika Yerawar <avantikayerawar517@gmail.com>
2026-03-02 23:51:58 +0530

    update

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,4 +1,4 @@
-
+const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
 import { IUpdatePost, INewPost, INewUser, IUpdateUser } from "@/types";
 
 // ============================== CREATE USER ACCOUNT (Spring Boot)
